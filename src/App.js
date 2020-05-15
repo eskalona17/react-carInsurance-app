c162084226130734c8d436f5513d25e3d1097924
eskalona17 <eskalona_17@hotmail.com>
2020-05-15 11:27:36 +0200

    creating styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <h1>Hola mundo</h1>
-   
-  );
+  return <Header titulo="Cotizador de seguros" />;
 }
 
 export default App;
